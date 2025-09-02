eafa923a920edc14877b7c04dbb5b63557ba5472
agathavictoria899 <agathavictoria2236@gmail.com>
2025-09-02 20:52:00 -0300

    primeiro commit

---
+++ ola.py
@@ -0,0 +1,9 @@
+print("ola")
+nome = "agatha victoria"
+print("hello agatha")
+print(nome)
+#print (type (nome))
+idade = "14"
+usuario_ativo = True
+print((type)(int)(idade))
+print (type) (usuario_ativo)
